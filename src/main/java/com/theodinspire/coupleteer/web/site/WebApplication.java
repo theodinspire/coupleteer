fb9308d7f9783e3521fa22ab87ee37c4d1c0cecc
theodinspire <bobryuu@gmail.com>
2017-06-19 10:24:37 -0500

    Spring application set up

---
+++ src/main/java/com/theodinspire/coupleteer/web/site/WebApplication.java
@@ -0,0 +1,15 @@
+package com.theodinspire.coupleteer.web.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by corms on 6/19/17.
+ */
+
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
