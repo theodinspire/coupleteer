42feb13f60712695c62c31e6a75d65e3e4386644
theodinspire <bobryuu@gmail.com>
2017-06-20 20:06:43 -0500

    Static index page calling API

--- src/main/java/com/theodinspire/coupleteer/web/site/IndexController.java
+++ src/main/java/com/theodinspire/coupleteer/web/site/IndexController.java
@@ -1,26 +1,28 @@
-package com.theodinspire.coupleteer.web.site;
+/* Currently handled by a static page */
 
-import com.theodinspire.coupleteer.Couplet;
-import com.theodinspire.coupleteer.Coupleteer;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-/**
- * Created by corms on 6/19/17.
- */
-
-@Controller
-public class IndexController {
-    
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
-        Couplet couplet = Coupleteer.getInstance().getCouplet();
-        String string = couplet.toStringWithDelimiter("<br />");
-        
-        model.addAttribute("couplet", string);
-        
-        return "index";
-    }
-}
+//package com.theodinspire.coupleteer.web.site;
+//
+//import com.theodinspire.coupleteer.Couplet;
+//import com.theodinspire.coupleteer.Coupleteer;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+///**
+// * Created by corms on 6/19/17.
+// */
+//
+//@Controller
+//public class IndexController {
+//
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    public String index(Model model) {
+//        Couplet couplet = Coupleteer.getInstance().getCouplet();
+//        String string = couplet.toStringWithDelimiter("<br />");
+//
+//        model.addAttribute("couplet", string);
+//
+//        return "index";
+//    }
+//}
