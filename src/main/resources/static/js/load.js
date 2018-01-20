bc5202d30b01b7302d7b681e96d7c519f325cef8
Eric T Cormack <bobryuu@gmail.com>
2018-01-20 00:19:17 -0600

    Basic styling finished and about page added

--- src/main/resources/static/js/load.js
+++ src/main/resources/static/js/load.js
@@ -3,17 +3,31 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function pullCouplet() {
-  var coupletLine1, coupletLine2;
+  var coupletLine1, coupletLine2, refreshButton;
 
-  coupletLine1 = document.getElementById('couplet');
-  coupletLine2 = coupletLine1.children[1];
-  coupletLine1 = coupletLine1.children[0];
+  document.body.className = 'loading';
+
+  coupletLine1 = document.getElementById('line-one');
+  coupletLine2 = document.getElementById('line-two');
+
+  refreshButton = document.getElementById('refresh');
 
   fetch('/api/couplet').then(function (res) {
     res.json().then(function (data) {
       document.body.className = null;
       coupletLine1.textContent = data.first;
       coupletLine2.textContent = data.second;
+
+      refreshButton.classList.remove('hidden');
     });
   });
+}
+
+function testPull() {
+  if (document.body.className == '') {
+    document.body.className = 'loading';
+  } else {
+    document.body.className = '';
+  }
+  document.getElementById('refresh').classList.remove('hidden');
 }
